6a11a2c50faa72e5c71c8e6b29b7d4f9629cd366
angganurf <angganurfaizal234@gmail.com>
2025-03-24 00:44:48 +0700

    update api/index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,12 @@
+require("dotenv").config();
+require("app-module-path").addPath(__dirname);
+// const serverless = require("serverless-http");
 const express = require("express");
-const app = express();
+const App = require("../dist/app").default;
 
-app.get("/", (req: any, res: { send: (arg0: string) => any }) =>
-	res.send("Express on Vercel")
-);
+const app = new App();
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+// app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
+module.exports.handler = express(app);
