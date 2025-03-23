f0566233db96489896004c020a1c66f035f0767b
angganurf <angganurfaizal234@gmail.com>
2025-03-24 00:36:04 +0700

    add vercel.json 6

--- api/index.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-require("app-module-path").addPath(__dirname);
-const serverless = require("serverless-http");
-const App = require("../dist/app").default;
-
-const app = new App();
-
-module.exports = app;
-module.exports.handler = serverless(app);
